fbc3c148c4edaf33bef5a743908bc2f12a8dc71a
Rômulo Souza Fernandes <rromulofer@gmail.com>
2025-03-19 09:39:15 -0300

    Update aula 1

--- BlazorShop/BlazorShop.Api/Context/AppDbContext.cs
+++ BlazorShop/BlazorShop.Api/Context/AppDbContext.cs
@@ -15,5 +15,25 @@ namespace BlazorShop.Api.Context
         public DbSet<Categoria>? Categorias { get; set; }
         public DbSet<Usuario>? Usuarios { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Categoria>().HasData(new Categoria
+             {
+                Id = 1,
+                Nome = "Beleza",
+                IconCSS = "fas fa-spa"
+              });
+            modelBuilder.Entity<Produto>().HasData(new Produto
+            {
+                Id=1,
+                Nome = "Glossier - Beleza Kit",
+                Descricao = "Um kit fornecido pela Natura, contendo produtos para pele",
+                ImagemUrl = "/Imagens/Beleza/Beleza1.png",
+                Preco = 100,
+                Quantidade = 100,
+                CategoriaId = 1
+            });
+            modelBuilder.Entity<Usuario>().HasData(new Usuario
+                )
     }
 }
